e2b4d6fdc96194ade287b2c2c3f26f259ce05cea
CHURAKOV Roman <roman.churakov@raiffeisen.ru>
2022-10-29 20:37:58 +0300

    создал бин Queue вместо бина RabbitAdmin

--- src/main/java/com/gmail/_4rl1996/queueedu/config/ConnectionFactoryConfig.java
+++ src/main/java/com/gmail/_4rl1996/queueedu/config/ConnectionFactoryConfig.java
@@ -4,7 +4,6 @@ import com.gmail._4rl1996.queueedu.config.properties.ApplicationProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -18,10 +17,8 @@ public class ConnectionFactoryConfig {
     private final ApplicationProperties applicationProperties;
 
     @Bean
-    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
-        final var rabbitAdmin = new RabbitAdmin(connectionFactory);
-        rabbitAdmin.declareQueue(new Queue(applicationProperties.getQueueName()));
-        return rabbitAdmin;
+    public Queue getQueue() {
+        return new Queue(applicationProperties.getQueueName());
     }
 
     @Bean
