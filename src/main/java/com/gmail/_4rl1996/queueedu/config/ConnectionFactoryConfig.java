ed4bc420f26aea51f7a6c0fe9ba9cad5c02fba03
CHURAKOV Roman <roman.churakov@raiffeisen.ru>
2022-11-03 19:51:49 +0300

    правки по ревью

--- src/main/java/com/gmail/_4rl1996/queueedu/config/ConnectionFactoryConfig.java
+++ src/main/java/com/gmail/_4rl1996/queueedu/config/ConnectionFactoryConfig.java
@@ -1,10 +1,6 @@
 package com.gmail._4rl1996.queueedu.config;
 
-import com.gmail._4rl1996.queueedu.config.properties.ApplicationProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,19 +10,6 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class ConnectionFactoryConfig {
 
-    private final ApplicationProperties applicationProperties;
-
-    @Bean
-    public Queue getQueue() {
-        return new Queue(applicationProperties.getQueueName());
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
-        return rabbitTemplate;
-    }
 
     @Bean
     public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
